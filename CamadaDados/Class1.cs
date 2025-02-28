c7e9cbcce4788576210ede2f967a7a0443c37b04
rafaelgrohl222 <rafaelgrohl222@gmail.com>
2025-02-27 22:15:23 -0400

    Sistema Comércio, com C# e SQL Server

---
+++ CamadaDados/Class1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CamadaDados
+{
+    public class Class1
+    {
+    }
+}
+
+//Aula 9, 2min
